ea24e971ea8097f157d176bc83d4bcfe32520ac3
Ilya Kotomin <mostlys4nny@gmail.com>
2022-12-11 16:45:02 +0200

    Added host model

--- DiscordArenaBot/Program.cs
+++ DiscordArenaBot/Program.cs
@@ -1,8 +1,21 @@
-﻿namespace DiscordArenaBot
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace DiscordArenaBot
 {
     public static class Programm
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
+        {
+            var configurationBuilder = new ConfigurationBuilder();
+            configurationBuilder.SetBasePath(AppContext.BaseDirectory);
+            configurationBuilder.AddJsonFile("appsettings.json");
+
+             using IHost host = Host.CreateDefaultBuilder()
+                .ConfigureServices((context, services) => ConfirurateServices(services)).Build();
+        }
+        private static void ConfirurateServices(IServiceCollection services)
         {
 
         }
