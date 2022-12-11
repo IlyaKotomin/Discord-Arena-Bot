1eea61014f3050309e769b13e4938b6225271600
Ilya Kotomin <mostlys4nny@gmail.com>
2022-12-11 18:56:31 +0200

    DB fixes

--- DiscordArenaBot/Data/BotDbContext.cs
+++ DiscordArenaBot/Data/BotDbContext.cs
@@ -4,12 +4,22 @@ using Microsoft.Extensions.Options;
 
 namespace DiscordArenaBot.Data
 {
-    internal class BotDbContext : DbContext
+    public class BotDbContext : DbContext
     {
         public BotDbContext(DbContextOptions<BotDbContext> options) : base(options)
         {
 
         }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Data Source=DESKTOP-01SD5F5;Initial Catalog=ARENA-BOT-DB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            }
+        }
+
+
         public DbSet<Player> Players { get; set; }
     }
 }
---
+++ DiscordArenaBot/Data/BotDbContextOptionsBuilder.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordArenaBot.Data
+{
+    public class BotDbContextOptionsBuilder : IDesignTimeDbContextFactory<BotDbContext>
+    {
+        public BotDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<BotDbContext>();
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-01SD5F5;Initial Catalog=ARENA-BOT-DB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+
+            return new BotDbContext(optionsBuilder.Options);
+        }
+    }
+}
