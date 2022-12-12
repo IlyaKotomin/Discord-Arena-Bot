a275a8e06a1fe718eb0d9da36db2cd6fd06a73a0
Ilya Kotomin <mostlys4nny@gmail.com>
2022-12-12 11:08:38 +0200

    Removed All migrations

--- DiscordArenaBot/Data/BotDbContext.cs
+++ DiscordArenaBot/Data/BotDbContext.cs
@@ -15,7 +15,7 @@ namespace DiscordArenaBot.Data
         {
             if (!optionsBuilder.IsConfigured)
             {
-                optionsBuilder.UseSqlServer("Data Source=DESKTOP-01SD5F5;Initial Catalog=ARENA-BOT-DB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+                optionsBuilder.UseSqlServer(Programm.Config[""]);
             }
         }
 
